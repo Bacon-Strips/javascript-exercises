b2c2f9e8d6ff711a428cb65540d81ebd4c2aab94
Bacon-Strips <spg.supeigeng@gmail.com>
2022-05-28 16:15:09 +0800

    Done practice 01 to 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    let result = arr;
+    for (let ele of toRemove) {
+        result = result.filter(stuff => stuff !== ele);
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof(num1) != 'number' || typeof(num2) != 'number' || num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    let between = Math.abs(num1 - num2);
+    let result = (between + 1) * Math.min(num1, num2);
+    for (let i = 1; i <= between; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
